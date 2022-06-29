510f853c9173a23acb190c04180d90092fc699d8
Matt Peachey <peacheym@dal.ca>
2022-06-29 10:21:56 -0300

    Move files for better directory structure.

--- ArduinoProject/ArduinoProject.ino
+++ ArduinoProject/ArduinoProject.ino
@@ -38,14 +38,28 @@ void loop() {
   float x, y, z;
 
   // make a string for assembling the data to log:
-  String dataString = "";
+  String dataString = "0,0,0";
 
 
   if (IMU.accelerationAvailable()) {
     IMU.readAcceleration(x, y, z);
-//    dataString = String(x) + "," + String(y) + "," + String(z);
-    dataString = String(x) + ",";
-
+    dataString = String(x) + "," + String(y) + "," + String(z);
     Serial.println(dataString);
   }
+
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+
+  // if the file is available, write to it:
+  if (dataFile) {
+    dataFile.println(dataString);
+    dataFile.close();
+    // print to the serial port too:
+    Serial.println(dataString);
+  }
+  // if the file isn't open, pop up an error:
+  else {
+    Serial.println("error opening datalog.txt");
+  }
+
+  
 }
