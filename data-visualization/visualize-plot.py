b0acaff69c31add893029d631c9ee7f81619b6b6
Matt Peachey <peacheym@dal.ca>
2022-06-30 10:21:50 -0300

    Begin working on 3D vis for all axes.

--- data-visualization/visualize-plot.py
+++ data-visualization/visualize-plot.py
@@ -13,5 +13,12 @@ for i in f:
     x_axis.append(line[0])
     y_axis.append(line[1])
     z_axis.append(line[2])
-plt.plot(y_axis)
+    
+
+fig, axs = plt.subplots(3, 1, sharex=True)
+    
+
+axs[0] = plt.plot(x_axis)
+axs[1] = plt.plot(y_axis)    
+axs[2] = plt.plot(z_axis)
 plt.show()
